3e9a726ed372e475725dd2ee5fb5ae29e62db1de
svp-gbn <144031985+svp-gbn@users.noreply.github.com>
2023-09-12 16:15:34 +0200

    Update procleny.js

--- procleny.js
+++ procleny.js
@@ -25,3 +25,102 @@ $(".form-dropdown").click(function () {
     } 
 }
     )
+
+
+
+
+     // Set the date we're counting down to
+     var countDownDate = new Date("Sep 12, 2023 16:09:25").getTime();
+     var countDownDate2 = new Date("Sep 12, 2023 16:10:25").getTime();
+
+     
+     // Update the count down every 1 second
+     var x = setInterval(function() {
+        var dateSet = true
+        $(".notset").hide();
+       // Get today's date and time
+       var now = new Date().getTime();
+         
+       // Find the distance between now and the count down date
+       var distance = countDownDate - now;
+         
+       // Time calculations for days, hours, minutes and seconds
+       var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+       var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+       var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+         
+       // Output the result in an element with id="demo"
+       document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+       + minutes + "m " + seconds + "s ";
+         
+       // If the count down is over, write some text 
+       if (distance < 0) {
+         clearInterval(x);
+         $(".hset").text("Schůze probíhá")
+         
+
+
+
+
+ // Update the count down every 1 second
+ var y = setInterval(function() {
+    // Get today's date and time
+    var now2 = new Date().getTime();
+      
+    // Find the distance between now and the count down date
+    var distance2 = countDownDate2 - now2;
+      
+    // Time calculations for days, hours, minutes and seconds
+    var days2 = Math.floor(distance2 / (1000 * 60 * 60 * 24));
+    var hours2 = Math.floor((distance2 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes2 = Math.floor((distance2 % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds2 = Math.floor((distance2 % (1000 * 60)) / 1000);
+      
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML = days2 + "d " + hours2 + "h "
+    + minutes2 + "m " + seconds2 + "s ";
+      
+    // If the count down is over, write some text 
+    if (distance2 < 0) {
+      clearInterval(x);
+
+      
+      dateSet = false;
+         $(".hset").text("Datum schůze ještě nebylo určeno")
+
+         $("#demo").hide();
+         $(".notset").show();
+    } 1000});
+
+
+
+
+
+
+
+
+         dateSet = false;
+         if(dateSet===true) {
+            $("#demo").show();
+            $(".notset").hide();
+        
+        } else if(dateSet===false) {
+
+            $("#demo").hide();
+            $(".notset").show();
+        }
+        
+
+       }
+     }, 1000);
+
+
+
+
+
+
+
+
+     
+
